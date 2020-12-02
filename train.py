master: new pgd attack, std normalization, fixes cyrcle lr

train.py
@@ -3,6 +3,10 @@ import jax
 import numpy as np
 import random
 import sys
+import wandb
+
+import tensorflow as tf
+from tensorflow.keras.mixed_precision import experimental as mixed_precision
 
 import flax.optim as optim
 import flax.jax_utils as flax_utils
@@ -18,10 +22,11 @@ from absl import flags
 FLAGS = flags.FLAGS
 
 flags.DEFINE_integer('batch_size', 128, '')
-flags.DEFINE_integer('number_epochs', 25, '')
+flags.DEFINE_integer('eval_batch_size', 10240, '')
+flags.DEFINE_integer('number_epochs', 15, '')
 flags.DEFINE_string('dataset', 'cifar10', '')
 flags.DEFINE_string('model', 'PreResNet18', 'architecture to use')
-flags.DEFINE_integer('test_every_steps', 100, '')
+flags.DEFINE_integer('test_every_steps', 500, '')
 flags.DEFINE_integer('save_every', 100, '')
 flags.DEFINE_string('checkpoint_name', './model.npz', '')
 flags.DEFINE_integer('crop_size', 32, '')
@@ -31,7 +36,6 @@ flags.DEFINE_float('eps', 8.0 / 255.0, '')
 flags.DEFINE_float('alpha', 10.0 / 255.0, '')
 flags.DEFINE_float('pgd_alpha', 2.0 / 255.0, '')
 flags.DEFINE_integer('pgd_restarts', 10, '')
-
 
 models = {
     'ResNet50': models.ResNet50,
@@ -46,9 +50,24 @@ models = {
     'PreResNet18': models.PreActResNet18,
 }
 
+cifar10_mean = np.array([0.4914, 0.4822, 0.4465])
+cifar10_std = np.array([0.2471, 0.2435, 0.2616])
+
+upper_limit = ((1 - cifar10_mean) / cifar10_std)
+lower_limit = ((0 - cifar10_mean) / cifar10_std)
+
 
 def main(argv):
     del argv
+
+    # On TPUs, use 'mixed_bfloat16' instead
+    # policy = tf.keras.mixed_precision.experimental.Policy('mixed_float16')
+    # mixed_precision.set_policy(policy)
+    #
+    # print('Compute dtype: %s' % policy.compute_dtype)
+    # print('Variable dtype: %s' % policy.variable_dtype)
+
+    wandb.init(project='smooth_adversarial')
 
     seed = random.Random().randint(0, sys.maxsize)
     rnd_key = jax.random.PRNGKey(seed)
@@ -57,7 +76,7 @@ def main(argv):
     batches_per_train = int(np.ceil(train_info['num_examples'] / FLAGS.batch_size))
 
     test_info = input_pipeline.get_dataset_info(FLAGS.dataset, 'test', examples_per_class=None)
-    batches_per_test = int(np.ceil(test_info['num_examples'] / FLAGS.batch_size))
+    batches_per_test = int(np.ceil(test_info['num_examples'] / FLAGS.eval_batch_size))
 
     train_steps = batches_per_train * FLAGS.number_epochs
     test_steps = batches_per_test
@@ -80,7 +99,7 @@ def main(argv):
     test_ds = input_pipeline.get_data(dataset=FLAGS.dataset,
                                       mode='test',
                                       repeats=None,
-                                      batch_size=FLAGS.batch_size,
+                                      batch_size=FLAGS.eval_batch_size,
                                       resize_size=FLAGS.resize_size,
                                       crop_size=FLAGS.crop_size,
                                       mixup_alpha=None,
@@ -105,14 +124,18 @@ def main(argv):
 
     # Create function for training
     lr_fn = utils.cyclic_lr(FLAGS.lr, train_steps)
+    alpha = FLAGS.alpha / cifar10_std
+    eps = FLAGS.eps / cifar10_std
+    pgd_alpha = FLAGS.pgd_alpha / cifar10_std
     update_fn = jax.pmap(functools.partial(utils.update_stateful,
-                                           eps=FLAGS.eps,
-                                           alpha=FLAGS.alpha),
+                                           eps=eps,
+                                           alpha=alpha),
                          axis_name='batch')
-    p_eval_step = jax.pmap(utils.eval_step, axis_name='batch')
+    p_eval_step = jax.pmap(utils.eval_step,
+                           axis_name='batch')
     p_eval_robust_step = jax.pmap(functools.partial(utils.robust_eval_step,
-                                                    eps=FLAGS.eps,
-                                                    pgd_alpha=FLAGS.pgd_alpha),
+                                                    eps=eps,
+                                                    pgd_alpha=pgd_alpha),
                                   axis_name='batch')
 
     # Metrics
@@ -121,44 +144,48 @@ def main(argv):
     losses = []
 
     # Start training
+    mean, std = [], []
     print('Number of steps', train_steps, flush=True)
     for step, batch in zip(range(1, train_steps + 1), train_ds.as_numpy_iterator()):
         # Generate a PRNG key that will be rolled into the batch
         rng, step_key = jax.random.split(rnd_key)
         # Shard the step PRNG key
         sharded_keys = common_utils.shard_prng_key(step_key)
+        curr_lr = lr_fn(step - 1)
+        sharded_lr = flax_utils.replicate(curr_lr)
+        a = np.array(batch['image'])
+        mean.append(np.mean(a.reshape((-1, 3)), axis=0))
+        std.append(np.std(a.reshape((-1, 3)), axis=0))
+        wandb.log({"lr": curr_lr},
+                  step=step)
         opt_repl, state_repl = update_fn(opt=opt_repl,
                                          batch=batch,
                                          state=state_repl,
                                          rnd_key=sharded_keys,
-                                         lr=flax_utils.replicate(lr_fn(step)))
+                                         lr=sharded_lr)
 
-        if step % FLAGS.test_every_steps == 0:
+        if step % FLAGS.test_every_steps == 0 or step == 1 or step == train_steps:
             loss, acc = [], []
             r_loss, r_acc = [], []
-            for idx, batch in zip(range(test_steps), test_ds.as_numpy_iterator()):
+            for batch_idx, batch in zip(range(test_steps), test_ds.as_numpy_iterator()):
                 metrics = p_eval_step(opt_repl.target,
                                       state_repl,
                                       batch)
-                r_metrics = None
-                for _ in range(FLAGS.pgd_restarts):
-                    r_m = p_eval_robust_step(opt=opt_repl,
-                                             state=state_repl,
-                                             batch=batch)
-                    r_m['loss'] = np.array(r_m['loss'])
-                    r_m['error_rate'] = np.array(r_m['error_rate'])
-                    if r_metrics is None:
-                        r_metrics = r_m
-                    else:
-                        for idx, (loss_new, loss_old) in enumerate(zip(r_m['loss'], r_metrics['loss'])):
-                            if loss_new > loss_old:
-                                r_metrics['loss'][idx] = loss_new
-                                r_metrics['error_rate'][idx] = r_m['error_rate'][idx]
+                r_metrics = p_eval_robust_step(opt=opt_repl,
+                                               state=state_repl,
+                                               rnd_key=sharded_keys,
+                                               batch=batch)
 
                 loss.append(metrics['loss'])
                 acc.append(1.0 - metrics['error_rate'])
                 r_loss.append(r_metrics['loss'])
                 r_acc.append(1.0 - r_metrics['error_rate'])
+
+            wandb.log({"loss": np.mean(loss),
+                       "acc": np.mean(acc),
+                       "robust_loss": np.mean(r_loss),
+                       "robust_acc": np.mean(r_acc)},
+                      step=step)
             print("Test on step", step,
                   "@loss:", np.mean(loss),
                   "@acc:", np.mean(acc),
@@ -169,8 +196,8 @@ def main(argv):
             accs.append(np.mean(acc))
             losses.append(np.mean(loss))
             steps.append(step)
+    print('final', np.mean(mean, axis=0), np.mean(std, axis=0))
 
 
 if __name__ == '__main__':
     app.run(main)
-

utils.py
@@ -5,7 +5,15 @@ import functools
 import io
 import numpy as np
 
+from jax.experimental import loops
+
 from cleverhans.future.jax.attacks import projected_gradient_descent as pgd
+
+cifar10_mean = np.array([0.4914, 0.4822, 0.4465])
+cifar10_std = np.array([0.2471, 0.2435, 0.2616])
+
+upper_limit = ((1 - cifar10_mean) / cifar10_std)
+lower_limit = ((0 - cifar10_mean) / cifar10_std)
 
 
 def save_ckpt(model_params, path):
@@ -27,65 +35,51 @@ def load_ckpt(tree, path):
 
 
 def cyclic_lr(base_lr, num_steps):
-  def step_fn(step):
-    N = num_steps//2
-    if step < N:
-      lr = base_lr * step/N
-    else:
-      lr = base_lr * (1.0 - (step%N)/N)
-    return lr
-
-  return step_fn
-
-
-def load_and_shard_tf_batch(xs):
-  local_device_count = jax.local_device_count()
-  def _prepare(x):
-    # Use _numpy() for zero-copy conversion between TF and NumPy.
-    # x = x.numpy()  # pylint: disable=protected-access
-    return x.reshape((local_device_count, -1) + x.shape[1:])
-
-  return jax.tree_map(_prepare, xs)
-
-
-def constant_lr(base_lr, num_steps, warmup=0):
     def step_fn(step):
-        return base_lr
+        assert step < num_steps
+        N = np.ceil(num_steps / 2)
+        if step < N:
+            lr = base_lr * step / N
+        else:
+            lr = base_lr * (1.0 - (step % N) / N)
+        return lr
 
     return step_fn
 
 
-def cross_entropy_loss(logits, labels):
+def load_and_shard_tf_batch(xs):
+    local_device_count = jax.local_device_count()
+
+    def _prepare(x):
+        return x.reshape((local_device_count, -1) + x.shape[1:])
+
+    return jax.tree_map(_prepare, xs)
+
+
+def constant_lr(base_lr, num_steps, warmup=0):
+    def step_fn(step):
+        del step
+        return base_lr
+
+    del num_steps
+    del warmup
+    return step_fn
+
+
+def cross_entory_loss_vec(logits, labels):
     logp = jax.nn.log_softmax(logits)
-    return -jnp.mean(jnp.sum(logp * labels, axis=1))
+    return jnp.sum(logp * labels, axis=1)
+
+
+def cross_entropy_loss(logits, labels):
+    # logp = jax.nn.log_softmax(logits)
+    # return -jnp.mean(jnp.sum(logp * labels, axis=1))
+    return -jnp.mean(cross_entory_loss_vec(logits, labels))
 
 
 def model_loss(params, images, labels, delta, model_fn):
     logits = model_fn(params, images + delta)
     return cross_entropy_loss(logits=logits, labels=labels)
-
-
-# # Update step, replicated over all GPUs
-# @partial(jax.pmap, axis_name='batch')
-def update(opt, rnd_key, lr, batch, loss_fn, eps, alpha):
-    rnd_key, rnd_input = jax.random.split(rnd_key)
-    delta = jax.random.uniform(key=rnd_input,
-                               shape=batch['image'].shape,
-                               minval=-eps,
-                               maxval=eps)
-    l, g_d = jax.value_and_grad(loss_fn, 3)(opt.target,
-                                            batch['image'],
-                                            batch['label'],
-                                            delta)
-    delta = jax.lax.clamp(-eps, delta + alpha * jnp.sign(g_d), eps)
-    l, g_t = jax.value_and_grad(loss_fn, 0)(opt.target,
-                                            batch['image'],
-                                            batch['label'],
-                                            delta)
-
-    g = jax.tree_map(lambda x: jax.lax.pmean(x, axis_name='batch'), g_t)
-    opt = opt.apply_gradient(g, learning_rate=lr)
-    return opt, rnd_key
 
 
 def evaluate(opt, images, labels, model_fn):
@@ -95,22 +89,93 @@ def evaluate(opt, images, labels, model_fn):
     return loss, acc
 
 
+def attack_pgd(opt, state, rnd_key, batch, eps, alpha,  attack_iters, restarts):
+    def loss_fn(model, delta):
+        with flax.nn.stateful(state) as new_state:
+            with flax.nn.stochastic(rnd_key):
+                logits = model(batch['image'] + delta)
+        loss = cross_entropy_loss(logits, batch['label'])
+        return loss
+
+    def loss_vec_fn(model, delta):
+        with flax.nn.stateful(state) as new_state:
+            with flax.nn.stochastic(rnd_key):
+                logits = model(batch['image'] + delta)
+        loss = cross_entory_loss_vec(logits, batch['label'])
+        return loss
+
+    X, y = batch['image'], batch['label']
+    max_loss = jnp.zeros((y.shape[0]))
+    max_delta = jnp.zeros_like(X)
+    with loops.Scope() as s:
+        for _ in s.range(restarts):
+            delta = init_delta(rnd_key, eps, batch)
+            for _ in s.range(attack_iters):
+                _, g_d = jax.value_and_grad(loss_fn, 1)(opt.target, delta)
+                delta = update_delta(X, delta, g_d, eps, alpha, X.shape[-1])
+
+            loss = -loss_vec_fn(opt.target, delta)
+            joint_loss = jnp.stack([max_loss, loss])
+
+            indexes = jnp.argmax(joint_loss, axis=0).reshape(-1, 1, 1, 1)
+            max_delta = max_delta * (1 - indexes) + delta * indexes
+            max_loss = jnp.maximum(max_loss, loss)
+            # max_delta = delta
+
+    return max_delta
+
+
 def calculate_delta(opt, state, rnd_key, batch, eps, alpha):
     def loss_fn(model, delta):
         with flax.nn.stateful(state) as new_state:
             with flax.nn.stochastic(rnd_key):
-                logits = model(batch['image'])
+                logits = model(batch['image'] + delta)
         loss = cross_entropy_loss(logits, batch['label'])
-        return loss, (new_state, logits)
+        return loss
 
     rnd_key, rnd_input = jax.random.split(rnd_key)
-    delta = jax.random.uniform(key=rnd_input,
-                               shape=batch['image'].shape,
-                               minval=-eps,
-                               maxval=eps)
-    grad_fn = jax.value_and_grad(loss_fn, 1, has_aux=True)
-    _, g_d = grad_fn(opt.target, delta)
-    delta = jax.lax.clamp(-eps, delta + alpha * jnp.sign(g_d), eps)
+    x = batch['image']
+    _, _, _, channels_number = x.shape
+    delta = init_delta(rnd_input, eps, batch)
+    _, g_d = jax.value_and_grad(loss_fn, 1)(opt.target, delta)
+
+    delta = update_delta(x, delta, g_d, eps, alpha, channels_number)
+    return delta
+
+
+def update_delta(x, delta, g_d, eps, alpha, channels_number):
+    deltas = []
+    for channel_idx in range(channels_number):
+        new_d = delta[..., channel_idx] + alpha[channel_idx] * jnp.sign(g_d)[..., channel_idx]
+        new_d = jax.lax.clamp(-eps[channel_idx], new_d, eps[channel_idx])
+
+        input = x[..., channel_idx]
+        lower = lower_limit[channel_idx] - input
+        upper = upper_limit[channel_idx] - input
+        new_d = jnp.maximum(jnp.minimum(new_d[:input.shape[0]], upper), lower)
+
+        deltas.append(new_d)
+    delta = jnp.stack(deltas, axis=-1)
+    return delta
+
+
+def init_delta(rnd_input, eps, batch):
+    deltas = []
+    x = batch['image']
+    b, h, w, channels_number = x.shape
+    for channel_idx in range(channels_number):
+        d = jax.random.uniform(key=rnd_input,
+                               shape=(b, h, w),
+                               minval=-eps[channel_idx],
+                               maxval=eps[channel_idx])
+
+        lower = lower_limit[channel_idx] - x[..., channel_idx]
+        upper = upper_limit[channel_idx] - x[..., channel_idx]
+
+        d = jnp.maximum(jnp.minimum(d, upper), lower)
+
+        deltas.append(d)
+    delta = jnp.stack(deltas, axis=-1)
     return delta
 
 
@@ -118,10 +183,9 @@ def update_stateful(opt, state, rnd_key, lr, batch, eps, alpha):
     def loss_fn(model, delta):
         with flax.nn.stateful(state) as new_state:
             with flax.nn.stochastic(rnd_key):
-                logits = model(batch['image'])
+                logits = model(batch['image'] + delta[:batch['image'].shape[0]])
         loss = cross_entropy_loss(logits, batch['label'])
         return loss, (new_state, logits)
-
     # Calculate distortion
     delta = calculate_delta(opt, state, rnd_key, batch, eps, alpha)
 
@@ -130,7 +194,6 @@ def update_stateful(opt, state, rnd_key, lr, batch, eps, alpha):
     (_, (new_state, logits)), grad = grad_fn(opt.target, delta)
     grad = jax.lax.pmean(grad, 'batch')
     new_opt = opt.apply_gradient(grad, learning_rate=lr)
-
     return new_opt, new_state
 
 
@@ -145,12 +208,14 @@ def compute_metrics(logits, labels):
     return metrics
 
 
-def robust_eval_step(opt, eps, pgd_alpha, state, batch):
+def robust_eval_step(opt, eps, rnd_key, pgd_alpha, state, batch):
     state = jax.lax.pmean(state, 'batch')
-    adv_x = pgd(opt.target, batch['image'], eps, pgd_alpha, 50, np.inf)
+    # adv_x = pgd(opt.target, batch['image'], eps, pgd_alpha, 50, np.inf)
+    delta = attack_pgd(opt, state, rnd_key, batch, eps, pgd_alpha, 50, 10)
     with flax.nn.stateful(state, mutable=False):
-        logits = opt.target(adv_x, train=False)
-    return compute_metrics(logits, batch['label'])
+        logits = opt.target(batch['image'] + delta, train=False)
+    metrics = compute_metrics(logits, batch['label'])
+    return metrics
 
 
 def eval_step(model, state, batch):
@@ -187,5 +252,3 @@ def eval_step(model, state, batch):
     with flax.nn.stateful(state, mutable=False):
         logits = model(batch['image'], train=False)
     return compute_metrics(logits, batch['label'])
-
-